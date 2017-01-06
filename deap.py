0186b942376c2dde36fc1340e841108cf4f2a6f2
ryusato2 <be.free.from.the.world@gmail.com>
2017-01-06 13:06:34 +0900

    Update deap.py

--- deap.py
+++ deap.py
@@ -11,13 +11,14 @@ from deap import creator
 from deap import tools
 from deap import gp
 
-# Define new functions
+# prog(f)/f(x)の計算不可を回避　（推定式/真の式）
 def safeDiv(left, right):
     try:
         return left / right
     except ZeroDivisionError:
         return 0
-
+#pset=primitive set, addPrimitiveでプリミティブ型データを乱数的に生成
+#pset.addEphemeralConstant...//のエラーが出たら削除
 pset = gp.PrimitiveSet("MAIN", 1)
 pset.addPrimitive(operator.add, 2)
 pset.addPrimitive(operator.sub, 2)
@@ -26,14 +27,16 @@ pset.addPrimitive(safeDiv, 2)
 pset.addPrimitive(operator.neg, 1)
 pset.addPrimitive(math.cos, 1)
 pset.addPrimitive(math.sin, 1)
-pset.addEphemeralConstant("rand101", lambda: random.randint(-1,1))
+pset.addEphemeralConstant("rand101", lambda: random.randint(-1,1))　#微量の値を定数として置く（"f(x)=x**4 - x**3 - x**2 - x"だと定数が０のため淘汰が早い）
 pset.renameArguments(ARG0='x')
 
+#１最小値を適合とする　　２個々の式を木構造で配列（生成）
 creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
 creator.create("Individual", gp.PrimitiveTree, fitness=creator.FitnessMin)
 
 
-
+#{indivisual|(x) population}の集団を作成
+#toolboxは諸関数をライブラリから呼び出す*二段下も参照*
 toolbox = base.Toolbox()
 toolbox.register("expr", gp.genHalfAndHalf, pset=pset, min_=1, max_=2)
 toolbox.register("individual", tools.initIterate, creator.Individual, toolbox.expr)
@@ -41,12 +44,13 @@ toolbox.register("population", tools.initRepeat, list, toolbox.individual)
 toolbox.register("compile", gp.compile, pset=pset)
 
 def evalSymbReg(individual, points):
-    # tree表現から関数への変換
+    # 木構造表現から関数への変換
     func = toolbox.compile(expr=individual)
     # 推定式と真の式との平均平方誤差の計算
     sqerrors = ((func(x) - x**4 - x**3 - x**2 - x)**2 for x in points)
     return math.fsum(sqerrors) / len(points),
 
+#選択、交差、突然変異を表現
 toolbox.register("evaluate", evalSymbReg, points=[x/10. for x in range(-10,10)])
 toolbox.register("select", tools.selTournament, tournsize=3)
 toolbox.register("mate", gp.cxOnePoint)
@@ -72,6 +76,8 @@ def main():
                                    halloffame=hof, verbose=True)
     # logの表示
     return pop, log, hof
+
+#モジュール内限定で実行
 if __name__ == "__main__":
     main()
 
